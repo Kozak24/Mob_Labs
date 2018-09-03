df3104f277f967daed66c4f9ebdf736cfdf5daef
Kozak24 <cossacko@ukr.net>
2018-09-03 11:15:54 +0300

    Recreated project

---
+++ app/src/main/java/kozak/labs/MainActivity.java
@@ -0,0 +1,19 @@
+package kozak.labs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        printIntoConsole();
+    }
+
+    private void printIntoConsole(){
+        System.out.println("Hello world");
+    }
+}
